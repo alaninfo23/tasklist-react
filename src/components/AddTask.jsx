c5e8b38ba25d14d13a12f8ebb6f5505bb672c90c
Alan Santos <js-alan@hotmail.com>
2023-02-08 11:33:15 -0300

    first commit

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,35 @@
+/* Comando imr importa a biblioteca */
+import React, { useState } from 'react';
+
+import Button from './Button';
+
+import './AddTask.css'
+
+/* Comando sfc cria uma função */
+const AddTask = ({ handleTaskAddition }) => {
+    const [inputData, setInputData] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputData(e.target.value);
+    }
+    const handleAddTaskClick = () => {
+        handleTaskAddition(inputData);
+        setInputData('');
+    }
+    return (
+        <div className='add-task-container'>
+            <input 
+                onChange={handleInputChange}
+                value={inputData}
+                className='add-task-input' 
+                type='text' 
+            />
+            ;
+            <div className="add-task-button-container"> 
+            <Button onClick={handleAddTaskClick}>Adicionar</Button>
+            </div>
+        </div>
+    );
+};
+ 
+export default AddTask;
---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,33 @@
+import { logDOM } from '@testing-library/react';
+import React from 'react';
+import { CgLaptop } from 'react-icons/cg';
+import { useHistory, useParams } from 'react-router-dom';
+import Button from './Button';
+
+import './AddTask.css';
+
+const TaskDetails = () => {
+    const params = useParams();
+    const history = useHistory();
+
+    const handleBackButtonClick = () => {
+        history.goBack();
+    }
+    
+    return ( 
+        <>
+            <div className="back-button-container">
+                <Button onClick={handleBackButtonClick}>Voltar</Button>
+            </div>
+            <div className="task-details-container">
+            <h2>{params.taskTitle}</h2>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit volu
+                    doloremque laudantium, totam rem aperiam, eaque ipsa q
+                    veritatis et quasi architecto beatae vitae 
+                </p>
+            </div>
+        </>
+     );
+}
+ 
+export default TaskDetails;
