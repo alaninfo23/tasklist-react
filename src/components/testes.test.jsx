4306fba24272ccc4a7341bf0c75ceceea7e4e6ec
Alan Santos <js-alan@hotmail.com>
2023-02-23 15:19:20 -0300

    iniciando testes

---
+++ src/components/testes.test.jsx
@@ -0,0 +1,8 @@
+/* Importando biblioteca de renderização e screen(para localizar textos) */
+import { render, screen } from "@testing-library/react";
+/* Importando a lib userEvent para interagir com o input */
+import userEvent, {} from '@testing-library/user-event';
+/* Importando a função Checknumber do diretório atual*/
+import { App } from ".";
+
+describe('')
