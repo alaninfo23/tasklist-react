e07c5a984ce6579ce8460c231c476b037cb388ad
Alan Santos <js-alan@hotmail.com>
2023-02-23 15:58:43 -0300

    instalando testing library e atualizando npm

--- src/testes.test.jsx
+++ src/testes.test.jsx
@@ -1,8 +1,14 @@
 /* Importando biblioteca de renderização e screen(para localizar textos) */
 import { render, screen } from "@testing-library/react";
 /* Importando a lib userEvent para interagir com o input */
-import userEvent, {} from '@testing-library/user-event';
+//import userEvent, {} from '@testing-library/user-event';
 /* Importando a função Checknumber do diretório atual*/
 import { App } from ".";
 
-describe('')
+describe('Tasklist', () => {
+    test('renders a title', () => {
+        render(<App />)
+        const title = screen.getByText('Minhas Tarefas');
+        expect(title).toBeInTheDocument();
+    });
+});
